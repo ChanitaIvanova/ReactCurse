ef2b9fcef280f741536300f8c7aca3dc6a57fbe2
ChanitaIvanova <chanita.ivanova@gmail.com>
2020-01-26 12:56:12 +0200

    Fetch from Server

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -4,6 +4,7 @@ import { Card, CardImg, CardText, CardBody,
     import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 class CommentForm extends Component {
   constructor(props) {
@@ -100,7 +101,7 @@ function RenderDish({dish}) {
   if (dish != null) {
      return(
          <Card>
-             <CardImg top src={dish.image} alt={dish.name} />
+             <CardImg top src={baseUrl + dish.image} alt={dish.name} />
              <CardBody>
                <CardTitle>{dish.name}</CardTitle>
                <CardText>{dish.description}</CardText>
