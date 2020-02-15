27642646614972ac803361c08952635b079d50bc
ChanitaIvanova <chanita.ivanova@gmail.com>
2020-02-15 12:37:08 +0200

    Icons and Buttons

--- confusionNative/App.js
+++ confusionNative/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { View } from 'react-native';
 import Main from './components/MainComponent';
+import * as Font from 'expo-font';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+     isFontLoaded: false
+   };
+  }
+  async componentWillMount() {
+    await Font.loadAsync({ 'MaterialIcons': require('react-native-vector-icons/Fonts/MaterialIcons.ttf'),
+  'FontAwesome': require('react-native-vector-icons/Fonts/FontAwesome.ttf') })
+    this.setState({ isFontLoaded: true })
+  }
+
   render() {
-    return (
-      <Main />
-    );
+    if (this.state.isFontLoaded) {
+      return (<Main />);
+    } else {
+      return(<View/>);
+    }
   }
 }
