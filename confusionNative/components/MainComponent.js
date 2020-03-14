df3d78c384edc85b9542715494577881dd462d93
ChanitaIvanova <chanita.ivanova@gmail.com>
2020-03-14 12:54:33 +0200

    Secure Store

--- confusionNative/components/MainComponent.js
+++ confusionNative/components/MainComponent.js
@@ -11,6 +11,7 @@ import { connect } from 'react-redux';
 import { fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/ActionCreators';
 import Reservation from './ReservationComponent';
 import Favorites from './FavoriteComponent';
+import Login from './LoginComponent';
 
 const mapStateToProps = state => {
   return {
@@ -135,6 +136,22 @@ const FavoritesNavigator = createStackNavigator({
         onPress={ () => navigation.navigate('DrawerToggle') } />
     })
 });
+const LoginNavigator = createStackNavigator({
+    Login: { screen: Login }
+  }, {
+  navigationOptions: ({ navigation }) => ({
+    headerStyle: {
+        backgroundColor: "#512DA8"
+    },
+    headerTitleStyle: {
+        color: "#fff"
+    },
+    headerTintColor: "#fff",
+    headerLeft: <Icon name="menu" size={24}
+      iconStyle={{ color: 'white' }}
+      onPress={ () => navigation.toggleDrawer() } />
+  })
+});
 const CustomDrawerContentComponent = (props) => (
   <ScrollView>
     <SafeAreaView style={styles.container} forceInset={{ top: 'always', horizontal: 'never' }}>
@@ -152,6 +169,21 @@ const CustomDrawerContentComponent = (props) => (
 );
 
 const MainNavigator = createDrawerNavigator({
+  Login:
+    { screen: LoginNavigator,
+      navigationOptions: {
+        title: 'Login',
+        drawerLabel: 'Login',
+        drawerIcon: ({ tintColor, focused }) => (
+          <Icon
+            name='sign-in'
+            type='font-awesome'
+            size={24}
+            iconStyle={{ color: tintColor }}
+          />
+        ),
+      }
+    },
     Home:
       { screen: HomeNavigator,
         navigationOptions: {
@@ -243,6 +275,7 @@ const MainNavigator = createDrawerNavigator({
           }
         }
 }, {
+  initialRouteName: 'Home',
   drawerBackgroundColor: '#D1C4E9',
   contentComponent: CustomDrawerContentComponent
 });
