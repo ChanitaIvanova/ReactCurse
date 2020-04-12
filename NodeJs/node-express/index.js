6cfb25e52b9d6dcd6ce94122689c7c9fb7ef7aa3
ChanitaIvanova <chanita.ivanova@gmail.com>
2020-04-12 17:50:36 +0300

    Assignment 1

--- NodeJs/node-express/index.js
+++ NodeJs/node-express/index.js
@@ -17,6 +17,14 @@ const dishRouter = require('./routes/dishRouter');
 
 app.use('/dishes', dishRouter);
 
+const promoRouter = require('./routes/promoRouter');
+
+app.use('/promotions', promoRouter);
+
+const leaderRouter = require('./routes/leaderRouter');
+
+app.use('/leaders', leaderRouter);
+
 const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
